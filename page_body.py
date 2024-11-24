102d6b387bd816c24c3bca70c33db6841c2a0a96
horacioneri <horacio.neri@ltplabs.com>
2024-11-24 22:43:50 +0000

    Add model interpretation

--- page_body.py
+++ page_body.py
@@ -9,7 +9,7 @@ from sklearn.utils.class_weight import compute_sample_weight
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, explained_variance_score
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve, auc, log_loss
 from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.inspection import PartialDependenceDisplay
+from sklearn.inspection import PartialDependenceDisplay, permutation_importance
 import statsmodels.api as sm
 import shap
 import plotly.express as px
