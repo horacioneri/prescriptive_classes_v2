03ff6c763e4ea28330e860d8c068d17aa954cf12
horacioneri <horacio.neri@ltplabs.com>
2024-11-24 22:55:16 +0000

    Add model interpretation

--- page_body.py
+++ page_body.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, e
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve, auc, log_loss
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.inspection import PartialDependenceDisplay, permutation_importance
-import matplotlib
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import shap
 import plotly.express as px
