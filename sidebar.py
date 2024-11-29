64ffd2c03f45c21275e39306b9ca76a4c65f5599
horacioneri <horacio.neri@ltplabs.com>
2024-11-29 08:37:23 +0000

    Keep previous selection in sidebar page 2

--- sidebar.py
+++ sidebar.py
@@ -70,7 +70,7 @@ def sidebar_config(i):
             missing_treat = st.selectbox(
                 'How to treat missing values:',
                 options,
-                index=0 if 'missing_treat' not in st.session_state else options.missing_treat
+                index=0 if 'missing_treat' not in st.session_state else options.index(missing_treat)
             )
             st.session_state.missing_treat = missing_treat
 
@@ -80,7 +80,7 @@ def sidebar_config(i):
             outlier_treat = st.selectbox(
                 'How to treat outlier values:',
                 options,
-                index=0 if 'outlier_treat' not in st.session_state else options.outlier_treat
+                index=0 if 'outlier_treat' not in st.session_state else options.index(outlier_treat)
             )
             st.session_state.outlier_treat = outlier_treat
 
