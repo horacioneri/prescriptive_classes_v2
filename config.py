31648cf9ea37485f07ffd804d4ec5b2d96f776c2
horacioneri <horacio.neri@ltplabs.com>
2025-05-07 18:02:43 +0100

    added login

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 
-page_titles = ['Introduction', 'Exploratory data analysis', 'Data preparation', 'Model training', 'Results analysis', 'Results interpretation', 'Summary']
+page_titles = ['Login', 'Introduction', 'Exploratory data analysis', 'Data preparation', 'Model training', 'Results analysis', 'Results interpretation', 'Summary']
